205e73343972922b9554f23a353305d96bc47f6b
george-shammar <george.gbenle@outlook.com>
2021-07-06 13:52:01 +0000

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <header className="App-header">
+        <p>Calculator</p>
+      </header>
     </div>
   );
 }
