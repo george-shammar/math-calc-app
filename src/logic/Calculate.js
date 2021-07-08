be411145dfca243bcd67dd2ba5eeb8bfebc578ac
george-shammar <george.gbenle@outlook.com>
2021-07-08 20:06:52 +0000

    Make corrections to calculate function

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -1,10 +1,13 @@
 import Big from 'big.js';
 import operate from './Operate';
 
-const calculate = (buttonName, data, operation) => {
-  if (buttonName == "+/-") {
-      const newTotal = data.total * -1;
-      const newNext = data.next * -1;
+const calculate = (buttonName, data) => {
+  if (buttonName === "+/-") {
+      const newTotal = new Big(data.total * -1);
+      const newNext = new Big(data.next * -1);
+      return data;
+  } else if (buttonName === ".") {
+
   }
 };
 
