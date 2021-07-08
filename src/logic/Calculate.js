aadd1d2eafb5650f6370950fe4b1aac754ec4d50
george-shammar <george.gbenle@outlook.com>
2021-07-08 19:55:59 +0000

    Add operation as parameter to calculate function

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -1,7 +1,7 @@
 import Big from 'big.js';
 import operate from './Operate';
 
-const calculate = (buttonName, data) => {
+const calculate = (buttonName, data, operation) => {
   if (buttonName == "+/-") {
       const newTotal = data.total * -1;
       const newNext = data.next * -1;
