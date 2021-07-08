161ab688a53e132633ea98782cad0671a4eed02f
george-shammar <george.gbenle@outlook.com>
2021-07-08 20:08:40 +0000

    Define data object

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -2,6 +2,7 @@ import Big from 'big.js';
 import operate from './Operate';
 
 const calculate = (buttonName, data) => {
+  const dataObject = { total: 0, next: 0, operation: '' };
   if (buttonName === "+/-") {
       const newTotal = new Big(data.total * -1);
       const newNext = new Big(data.next * -1);
