06d7bf28b3f8ab8cfee79b89fa444f8b416a88b7
george-shammar <george.gbenle@outlook.com>
2021-07-08 21:50:27 +0000

    Convert stateless App component to class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,15 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/Calculate'; // eslint-disable-line no-unused-vars
 
-function App() {
-  return (
-    <>
-      <Display result="0" />
-      <ButtonPanel />
-
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Display result="0" />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
