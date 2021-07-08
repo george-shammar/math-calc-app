2cf67af50fb7508bae9ffb55eca35ccc42ec39e6
george-shammar <george.gbenle@outlook.com>
2021-07-08 20:49:35 +0000

    Refactor operate function

--- src/logic/Operate.js
+++ src/logic/Operate.js
@@ -4,23 +4,19 @@ const operate = (numberOne, numberTwo, operation) => {
     const result = '';
     if (operation === '+') {
         result = new Big(numberOne + numberTwo);
-        return result;
     } else if (operation === '-') {
         result = new Big(numberOne - numberTwo);
-        return result;
     } else if (operation === 'X') {
         result = new Big(numberOne * numberTwo);
-        return result;
     } else if (operation === '÷') {
         result = new Big(numberOne/numberTwo);
-        return result;
     } else if (operation === '%') {
         result = new Big(numberOne/100);
-        return result;
     } else {
         result = '0';
-        return result;
     }
+
+    return result.toString();
 }
 
 export default operate;
