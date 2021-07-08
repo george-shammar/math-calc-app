96b9346c1f32b17d2682c1862396da4057c07922
george-shammar <george.gbenle@outlook.com>
2021-07-08 23:20:19 +0000

    Change operators for operate function

--- src/logic/Operate.js
+++ src/logic/Operate.js
@@ -3,13 +3,13 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   let result = '';
   if (operation === '+') {
-    result = new Big(numberOne + numberTwo);
+    result = new Big(numberOne.plus(numberTwo));
   } else if (operation === '-') {
-    result = new Big(numberOne - numberTwo);
+    result = new Big(numberOne.minus(numberTwo));
   } else if (operation === 'X') {
     result = new Big(numberOne * numberTwo);
   } else if (operation === '÷') {
-    result = new Big(numberOne / numberTwo);
+    result = new Big(numberOne.div(numberTwo));
   } else if (operation === '%') {
     result = new Big(numberOne / 100);
   } else {
