502e154e82a14d1ab6f6178e8d36065ad3879fcb
george-shammar <george.gbenle@outlook.com>
2021-07-14 17:05:32 +0000

    Import app.css in App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/Calculate';
+import '../stylesheets/app.css';
 
 class App extends React.Component {
   constructor(props) {
