f03da11619400fb3b763dd5d8b432dd3f457b975
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:26:15 +0000

    Display navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
-        <div>
-          <Link to="/">Home </Link>
-          <Link to="/calculator">Calculator </Link>
-          <Link to="/quote">Quote </Link>
-        </div>
-    );
-};
+  return (
+    <div>
+      <Link to="/">Home </Link>
+      <Link to="/calculator">Calculator </Link>
+      <Link to="/quote">Quote </Link>
+    </div>
+  );
+}
 
 export default Navbar;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,16 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Quote from './Quote';
 import App from './App';
-import Navbar from "./Navbar";
+import Navbar from './Navbar';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/calculator" component={App} />
-      <Route exact path="/quote" component={Quote} />
-    </Switch>
-  </BrowserRouter>
+  <div>
+    <nav>
+      <Navbar />
+    </nav>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={App} />
+        <Route exact path="/quote" component={Quote} />
+      </Switch>
+    </BrowserRouter>
+  </div>
 );
 
 export default Routes;
