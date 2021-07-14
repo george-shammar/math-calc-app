e7a8cda1426ffec03d5605290403d9f391cb2fe4
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:41:30 +0000

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,11 @@ function Navbar() {
   return (
     <div className="nav-container">
       <h1>Math Magicians</h1>
-      <Link to="/">Home </Link>
-      <Link to="/calculator">Calculator </Link>
-      <Link to="/quote">Quote </Link>
+      <div>
+        <Link to="/" className="links">Home </Link>
+        <Link to="/calculator" className="links">Calculator </Link>
+        <Link to="/quote" className="links">Quote </Link>
+      </div>
     </div>
   );
 }
