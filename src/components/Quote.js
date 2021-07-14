5eba61e31586502e66b9e462d9e53b34c589c82d
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:19:22 +0000

    Fix linter errors

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Quote = () => {
-    return(
-        <div>
-            <p>
-              Mathematics is not about numbers, equations, computations,
-              or algorithms: it is about understanding. –William Paul Thurston
-            </p>
-        </div>
-    )
-}
+const Quote = () => (
+  <div>
+    <p>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding. –William Paul Thurston
+    </p>
+  </div>
+);
 
 export default Quote;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import Quote from "./Quote";
-import App from "./App";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Quote from './Quote';
+import App from './App';
 
-const Routes = () => {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/calculator" component={App} />
-          <Route path="/quote" component={Quote} />
-        </Switch>
-      </BrowserRouter>
-    );
-  };
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/calculator" component={App} />
+      <Route path="/quote" component={Quote} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
