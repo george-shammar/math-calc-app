b400d0ee9c85e9210d4bbf0c23a57117d8282a6e
george-shammar <george.gbenle@outlook.com>
2021-07-14 17:49:12 +0000

    Import buttonpanel.css in ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../stylesheets/buttonpanel.css';
 
 const ButtonPanel = (props) => {
   const { clickHandler } = props;
