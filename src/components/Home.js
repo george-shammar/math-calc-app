12a5fbfe925e3db73c4a25a92693177f9925bafc
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:15:19 +0000

    Create Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,26 @@
 import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Math Magician</h1>
+            <h2>Welcome to our page!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Utenim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt inculpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis 
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu 
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+                inculpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+            </p>
+        </div>
+    )
+}
