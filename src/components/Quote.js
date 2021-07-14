185952823508c5ba016db320f67477ddcb1446a7
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:18:46 +0000

    Create Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const Quote = () => {
+    return(
+        <div>
+            <p>
+              Mathematics is not about numbers, equations, computations,
+              or algorithms: it is about understanding. –William Paul Thurston
+            </p>
+        </div>
+    )
+}
+
+export default Quote;
