fdc25a3f8792561ce290cea56d0be918e2bc75d8
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:08:31 +0000

    Import App component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Quote from "./Quote";
+import App from "./App";
 
 const Routes = () => {
     return (
       <BrowserRouter>
         <Switch>
           <Route path="/" component={Home} />
-          <Route path="/calculator" component={Calculator} />
+          <Route path="/calculator" component={App} />
           <Route path="/quote" component={Quote} />
         </Switch>
       </BrowserRouter>
