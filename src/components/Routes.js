2afca888f1a4fff3be99c47df04f762a51830720
george-shammar <george.gbenle@outlook.com>
2021-07-14 21:06:54 +0000

    Import components

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Quote from "./Quote";
 import Home from "./Home";
+import Quote from "./Quote";
 
 const Routes = () => {
     return (
