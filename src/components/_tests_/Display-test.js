ee00f03d1273ee2fdafffa19cff1138ea2868908
george-shammar <george.gbenle@outlook.com>
2021-07-15 21:30:24 +0000

    Write test

--- src/components/_tests_/Display-test.js
+++ src/components/_tests_/Display-test.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { shallow } from "enzyme";
+import Display from '../Display';
 
 describe("Display", () => {
-   
-  
-    
+    let wrapper;
+    it("wraps content in a div with .bg-display class", () => {
+        wrapper = shallow(<Display />);
+    });
 });
   
