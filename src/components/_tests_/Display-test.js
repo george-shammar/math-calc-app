bc32c2b97507c3c67d3444c5a0fe364e76f247a8
george-shammar <george.gbenle@outlook.com>
2021-07-15 21:31:09 +0000

    Write test

--- src/components/_tests_/Display-test.js
+++ src/components/_tests_/Display-test.js
@@ -6,6 +6,7 @@ describe("Display", () => {
     let wrapper;
     it("wraps content in a div with .bg-display class", () => {
         wrapper = shallow(<Display />);
+        expect(wrapper.find(".bg-display").length).toEqual(1);
     });
 });
   
