0b003ef90dcdd6a7091236728a2a6429c11f3dbc
george-shammar <george.gbenle@outlook.com>
2021-07-15 22:09:48 +0000

    Refactor display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,10 +4,13 @@ import '../stylesheets/display.css';
 const Display = (props) => {
   const { result, operation } = props;
   return (
-    <div className="bg-display">
-      <h1 className="result">{result}</h1>
-      <p>{operation}</p>
-    </div>
+    <div>
+      <h1 className="title">Calculator</h1>
+      <div className="bg-display">
+        <h1 className="result">{result}</h1>
+        <p>{operation}</p>
+      </div>
+     </div>
   );
 };
 
--- src/components/__tests__/Display-test.js
+++ src/components/__tests__/Display-test.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow, mount } from 'enzyme';
 import Display from '../Display';
 
-describe("Display", () => {
-    let wrapper;
-    it("wraps content in a div with .bg-display class", () => {
-        wrapper = shallow(<Display />);
-        expect(wrapper.find(".bg-display").length).toEqual(1);
-    });
+describe('Display', () => {
+  let wrapper;
+  it('wraps content in a div with .bg-display class', () => {
+    wrapper = shallow(<Display />);
+    expect(wrapper.find('.bg-display').length).toEqual(1);
+  });
 
-    it("must show valid contents and NOT null", () => {
-        wrapper = shallow(<Display />);
-        expect(wrapper.find(".bg-display").length).not.toEqual(null);
-    });
+  it('must show valid contents and NOT null', () => {
+    wrapper = shallow(<Display />);
+    expect(wrapper.find('.bg-display').length).not.toEqual(null);
+  });
 
-    it("must display valid contents that is defined", () => {
-        wrapper = shallow(<Display />);
-        expect(wrapper.find(".bg-display").length).toBeDefined;
-    });
+  it('must display valid contents that is defined', () => {
+    wrapper = shallow(<Display />);
+    expect(wrapper.find('.bg-display').length).toBeDefined;
+  });
+
+  it('must be truthy', () => {
+    wrapper = shallow(<Display />);
+    expect(wrapper.find('.bg-display').length).toBeTruthy;
+  });
 });
-  
