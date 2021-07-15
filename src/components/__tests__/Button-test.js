c55b48ccc02d0479667109da3ab9e7a4ce7d0c31
george-shammar <george.gbenle@outlook.com>
2021-07-15 23:30:01 +0000

    Create button panel test suite

--- src/components/__tests__/Button-test.js
+++ src/components/__tests__/Button-test.js
@@ -3,25 +3,25 @@ import { shallow } from 'enzyme';
 import Button from '../Button';
 
 describe('Button', () => {
-    let wrapper;
+  let wrapper;
 
-    it('wraps content in a div with .button class', () => {
-      wrapper = shallow(<Button />);
-      expect(wrapper.find('.button').length).toEqual(1);
-    });
-  
-    it('must show valid contents and NOT null', () => {
-      wrapper = shallow(<Button />);
-      expect(wrapper.find('.button').length).not.toEqual(null);
-    });
-  
-    it('must display valid contents that is defined', () => {
-      wrapper = shallow(<Button />);
-      expect(wrapper.find('.button').length).toBeDefined;
-    });
-  
-    it('must be truthy', () => {
-      wrapper = shallow(<Button />);
-      expect(wrapper.find('.button').length).toBeTruthy;
-    });
+  it('wraps content in a div with .button class', () => {
+    wrapper = shallow(<Button />);
+    expect(wrapper.find('.button').length).toEqual(1);
   });
+
+  it('must show valid contents and NOT null', () => {
+    wrapper = shallow(<Button />);
+    expect(wrapper.find('.button').length).not.toEqual(null);
+  });
+
+  it('must display valid contents that is defined', () => {
+    wrapper = shallow(<Button />);
+    expect(wrapper.find('.button').length).toBeDefined;
+  });
+
+  it('must be truthy', () => {
+    wrapper = shallow(<Button />);
+    expect(wrapper.find('.button').length).toBeTruthy;
+  });
+});
