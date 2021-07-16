94b7165be5428ad2b5a72df5db7785ecdfdde3df
george-shammar <george.gbenle@outlook.com>
2021-07-16 19:41:19 +0000

    Write test for App component

--- src/components/test/Calculator.test.js
+++ src/components/test/Calculator.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Display from '../Display';
+import App from '../App';
 
 it('renders the Display component correctly', () => {
     const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders the App component correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders the App component correctly with result props', () => {
+    const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
 });
