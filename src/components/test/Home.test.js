ab6a1c53080832018d81c1787ed4776190da6c47
george-shammar <george.gbenle@outlook.com>
2021-07-16 19:22:54 +0000

    Wrote test for Home component

--- src/components/test/Home.test.js
+++ src/components/test/Home.test.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../Home';
+
+it('renders the Home component correctly', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
